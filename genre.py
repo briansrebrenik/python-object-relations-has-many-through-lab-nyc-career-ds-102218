94afa623b07393ded7ee82153ee43eb93f897d1c
Brian Srebrenik <briansrebrenik@gmail.com>
2018-10-31 11:17:31 -0400

    finished lab

--- genre.py
+++ genre.py
@@ -6,8 +6,15 @@ class Genre:
     def __init__(self, name):
         self._name = name
 
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        self._name = name
+
     def songs(self):
-        pass
+        return[song.name for song in Song._all if song.genre == self]
 
     def artists(self):
-        pass
+        return[artist.name for song in Song._all if song.artist == self]
--- passenger.py
+++ passenger.py
@@ -1,16 +1,30 @@
 # import Trip class here
-
+from trip import Trip
 class Passenger:
 
     def __init__(self, name, age):
         self._name = name
         self._age = age
 
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def age(self):
+        return self._age
+    @age.setter
+    def age(self, age):
+        self._age = age
+
     def trips(self):
-        pass
+        return [trip for trip in Trip._all if trip.passenger == self]
 
     def drivers(self):
-        pass
+        return [trip.driver for trip in Trip._all if trip.passenger == self]
 
     def trip_count(self):
-        pass
+        return len(self.trips())
